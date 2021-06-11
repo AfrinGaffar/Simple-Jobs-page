7424a5087e9538eb4d63d85e12396159343cb82b
Afrin Begum <72100064+AfrinGaffar@users.noreply.github.com>
2021-06-12 00:29:04 +0800

    Add files via upload

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum atque ullam quo vitae dolores, eos dignissimos odio harum praesentium veritatis nam rem laboriosam fugiat minima ex molestiae incidunt aut architecto?</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum atque ullam quo vitae dolores, eos dignissimos odio harum praesentium veritatis nam rem laboriosam fugiat minima ex molestiae incidunt aut architecto?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum atque ullam quo vitae dolores, eos dignissimos odio harum praesentium veritatis nam rem laboriosam fugiat minima ex molestiae incidunt aut architecto?</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
